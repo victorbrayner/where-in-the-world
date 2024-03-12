089d57a0fb6cce0ce3fe90e505e001566334a465
Victor Brayner <victorbrayner@gmail.com>
2024-03-12 12:45:32 -0300

    feat: card details

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,11 @@
 <template>
   <q-page class="page flex q-pt-xl">
 
-    <SearchBar />
+    <SearchBar v-if="!store.openDetails" />
 
-    <CountriesCards />
+    <CountriesCards v-if="!store.openDetails" />
+
+    <CountryDetails v-else />
 
   </q-page>
 </template>
@@ -11,6 +13,10 @@
 <script setup>
 import SearchBar from 'src/components/SearchBar.vue'
 import CountriesCards from 'src/components/CountriesCards.vue'
+import CountryDetails from 'src/components/CountryDetails.vue'
+import { useCountryStore } from '../stores/country'
+
+const store = useCountryStore()
 
 </script>
 
