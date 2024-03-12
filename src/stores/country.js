c479579f44520e06d4c4c6fb17e18d4ddc5d5a1c
Victor Brayner <victorbrayner@gmail.com>
2024-03-12 12:22:35 -0300

    feat: defining pinia store

---
+++ src/stores/country.js
@@ -0,0 +1,29 @@
+import { defineStore } from "pinia";
+
+const api = "https://restcountries.com/v3.1";
+
+export const useCountryStore = defineStore("country", {
+  state: () => ({
+    allCountries: [],
+    searchedCountries: [],
+  }),
+  getters: {},
+  actions: {
+    async fetchCountries() {
+      const res = await fetch(`${api}/all`);
+      const json = await res.json();
+      this.allCountries = json;
+      this.searchedCountries = this.allCountries.slice(0, 25);
+    },
+    async fetchCountriesByName(name) {
+      const res = await fetch(`${api}/name/${name}`);
+      const json = await res.json();
+      this.searchedCountries = json;
+    },
+    async fetchCountriesByRegion(region) {
+      const res = await fetch(`${api}/region/${region}`);
+      const json = await res.json();
+      this.searchedCountries = json;
+    },
+  },
+});
